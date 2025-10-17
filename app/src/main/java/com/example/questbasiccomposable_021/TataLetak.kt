dc897eb3e2579312d980ccd68d40f8362c8e8d25
PannnTastic <pannntastic@gmail.com>
2025-10-17 15:37:27 +0700

    TataLetaxBox

--- app/src/main/java/com/example/questbasiccomposable_021/TataLetak.kt
+++ app/src/main/java/com/example/questbasiccomposable_021/TataLetak.kt
@@ -2,12 +2,15 @@ package com.example.questbasiccomposable_021
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -28,5 +31,20 @@ fun TataLetakRow(modifier: Modifier){
         Text(text = "Komponen2")
         Text(text = "Komponen3")
         Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataLetakBox(modifier: Modifier){
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .fillMaxHeight(), contentAlignment = Alignment.Center
+    ){
+        Text(text = "Box1")
+        Text(text = "Column1")
+        Text(text = "Row1")
+        Text(text = "Box2")
+        Text(text = "COlumn2")
     }
 }
