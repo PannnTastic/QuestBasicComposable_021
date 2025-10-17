bb5bb9e0183ebae9eff85c74f586620673f0f7b0
PannnTastic <pannntastic@gmail.com>
2025-10-17 16:00:14 +0700

    Add First Rown in Column

--- app/src/main/java/com/example/questbasiccomposable_021/TataLetak.kt
+++ app/src/main/java/com/example/questbasiccomposable_021/TataLetak.kt
@@ -101,7 +101,15 @@ fun TataLetakBoxColumnRow(modifier: Modifier){
                 .background(color = Color.Magenta),
             contentAlignment = Alignment.Center
         ){
-
+            Column {
+                Row(modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Col1_Row1_Komponen1")
+                    Text(text = "Col1_Row1_Komponen2")
+                    Text(text = "Col1_Row1_Komponen3")
+                }
+            }
         }
     }
 }
