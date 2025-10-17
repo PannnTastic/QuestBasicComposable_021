c44657d49755bf3cd846dc3dd7bd7056cc343473
PannnTastic <pannntastic@gmail.com>
2025-10-17 15:33:18 +0700

    TataLetakRow

--- app/src/main/java/com/example/questbasiccomposable_021/TataLetak.kt
+++ app/src/main/java/com/example/questbasiccomposable_021/TataLetak.kt
@@ -1,7 +1,10 @@
 package com.example.questbasiccomposable_021
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +14,16 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun TataLetakColumn(modifier: Modifier){
     Column(modifier = modifier.padding(top = 20.dp, start = 20.dp, end = 20.dp)) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
+@Composable
+fun TataLetakRow(modifier: Modifier){
+    Row(modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
         Text(text = "Komponen1")
         Text(text = "Komponen2")
         Text(text = "Komponen3")
