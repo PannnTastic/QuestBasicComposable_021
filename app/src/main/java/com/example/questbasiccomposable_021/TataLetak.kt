d7900e14b74b58aa56d67b7af9076f24fa53169d
PannnTastic <pannntastic@gmail.com>
2025-10-17 15:22:31 +0700

    add Composable

--- app/src/main/java/com/example/questbasiccomposable_021/TataLetak.kt
+++ app/src/main/java/com/example/questbasiccomposable_021/TataLetak.kt
@@ -1,2 +1,10 @@
 package com.example.questbasiccomposable_021
 
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TataLetakColumn(modifier: Modifier){
+
+}
