63462bbfded71d9a5b230d55901b9cd00cb05667
PannnTastic <pannntastic@gmail.com>
2025-10-17 16:08:31 +0700

    Add Text below Image in TataLetakBoxColumnRow

--- app/src/main/java/com/example/questbasiccomposable_021/TataLetak.kt
+++ app/src/main/java/com/example/questbasiccomposable_021/TataLetak.kt
@@ -18,7 +18,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TataLetakColumn(modifier: Modifier){
@@ -131,6 +134,14 @@ fun TataLetakBoxColumnRow(modifier: Modifier){
             Image(painter = gambar,
                 contentDescription = null,
                 contentScale = ContentScale.Fit)
+            Text(text = "Who's This Guy ?",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Serif,
+                modifier = Modifier.align(
+                    alignment = Alignment.Center
+                ))
         }
     }
 }
